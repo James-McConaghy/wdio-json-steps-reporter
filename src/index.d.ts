56ad990d840eb541254b4ddf16d01a94e4652544
unknown <james.mcconaghy@email.com>
2020-09-13 18:39:09 +0100

    Update types and method details

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,7 +10,7 @@ declare module "wdio-json-steps-reporter" {
         error?: string;
         screenshotPath?: string;
     }
-   
+
     type StepOptions = {
         createLog: boolean;
         takeScreenshot: boolean | WebdriverIO.Element;
@@ -19,29 +19,42 @@ declare module "wdio-json-steps-reporter" {
         customActual?: string;
     };
 
-    type Action = Step
-    type Assertion = Step
+    type Action = Function
+    type Assertion = Function
 
     /**
-     * The step wrapper should be used to generate a log and screenshot 
+     * The step (single) wrapper should be used to generate a log and screenshot 
      * for page object actions and assertions that the webdriver will execute. 
+     * 
+     * If nested step Actions/Assertions are used ensure to pass {createStep: false, takeScreenshot: false} to those steps, otherwise
+     * step logic will duplicate with this steps wrapping log
 
-     * @param {StepOptions} stepOptions to toggle logging and screenshot generation as well as overriding pre-defined page actions/assertions.
-     * @param {string} description of the code to be executed
-     * @param {string} expected outcome of the code to be executed
-     * @param {string} actual outcome of the code to be executed
-     * @param {Function} tasks to be executed by the webdriver
-     * @return {Action|Assertion} Returns itself as an Action|Assertion which can be used in the steps wrapper function
+        * @param {StepOptions} stepOptions to toggle logging and screenshot generation as well as overriding pre-defined page actions/assertions.
+        * @param {string} description of the code to be executed
+        * @param {string} expected outcome of the code to be executed
+        * @param {string} actual outcome of the code to be executed
+        * @param {Function} tasks to be executed by the webdriver
+        * @return {Action | Assertion} The Step log object that will be used in the resulting json results and combined Steps wrapper.
     */
     function step(stepOptions: StepOptions = { createLog: true, takeScreenshot: false }, description: string, expectation: string, actual: string, tasks: Function): Action | Assertion;
     
     /**
-     * The steps wrapper, similar to the singular step wrapper. Can be used to automatically combine step wrapper calls
-     * executed by the webdriver into a single log and screenshot in the process. 
-
-     * @param {StepOptions} stepOptions to toggle logging and screenshot generation as well as overriding pre-defined page actions/assertions.
-     * @param {Function[]} actions An array of step functions to be executed first.
-     * @param {Function[]} assertions An array of step functions to be executed after completing the actions.
+     * The steps (plural) wrapper, similar to the single step wrapper can be used to generate a single log and screenshot for multiple Actions/Assertions
+     * executed by the test. All actions will get executed first before any assertions are verified.
+     *
+     * Actions and Assertions used should accept the default stepOptions passed in via the callback, otherwise step logic
+     * will duplicate with this combined steps wrapping log. 
+     * 
+        * @param {StepOptions} stepOptions to toggle logging and screenshot generation as well as overriding pre-defined page actions/assertions.
+        * @param {Action[]} actions An array of step functions to be executed first.
+        * @param {Assertion[]} assertions An array of step functions to be executed after completing the actions.
+        * 
+        * @example steps({createLog: true, takeScreenshot: true},
+                    [(stepOptions: StepOptions) => navigateToPage("https://webdriver.io", stepOptions)], 
+                    [(stepOptions: StepOptions) => verifyPageTitle(stepOptions)]
+                )
+        * 
+        * 
     */
     function steps(stepOptions: StepOptions = { createLog: true, takeScreenshot: false }, actions: Action[], assertions: Assertion[]): void;
 
